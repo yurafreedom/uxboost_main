a7efca8e164b0878dc80ca908f01d987fe0859f1
Yura <kyrizan96@gmail.com>
2020-09-25 23:37:15 +0300

    new slider on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,3 @@
-// $(".page-header__nav-menu").on("click","a", function (event) {
-//     event.preventDefault();
-//     var id  = $(this).attr('href'),
-//         top = $(id).offset().top;
-//     $('body,html').animate({scrollTop: top}, 500);
-// });
-
-$(".page-header__toggle").click(function(){
-	// if ($(".page-header__nav-menu").hasClass('')) {
-	// 	$(".page-header__nav-menu").slideDown();
-	// } else {
-	// 	$(".page-header__nav-menu").slideUp();
-	// };
-
-	if ($(".page-header__nav-menu").hasClass('active')) {
-		$(".page-header__nav-menu").slideUp();
-	} else {
-		$(".page-header__nav-menu").slideDown();
-	};
-});
-
 $('[data-toggle="anchor"]').click(function(e) {
  e.preventDefault();
  
@@ -30,12 +9,10 @@ $('[data-toggle="anchor"]').click(function(e) {
  }, 800);
 });
 
-var $things = $(".nav-block");
-$things.waypoint(function(e) {
-    if ("down" === e) {
-        var t = this.element.id;
-        $(".page-header__nav a").removeClass("active"), $('[data-target="#' + t + '"]').addClass("active")
-    }
-}, {
-    offset: 151
+$('.intensives-block__slider').slick({
+  variableWidth: true,
+});
+
+$('.intensives-block__slider').on('afterChange', function(event, slick, currentSlide) {
+	$('.intensives-block__slider .slick-current').addClass('active').siblings().removeClass('active');
 });
